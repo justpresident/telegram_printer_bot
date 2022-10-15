86aba5fe12926ce3a875da383060db303bd28b92
Roman Studenikin <srv@fb.com>
2022-10-15 20:42:31 +0100

    Add a button for print confirmation

--- printerbot.py
+++ printerbot.py
@@ -4,22 +4,25 @@ import logging
 import os
 import pathlib
 import re
+import telegram
 from telegram import Update
-from telegram.ext import Updater
-from telegram.ext import ContextTypes
-from telegram.ext import CallbackContext
-from telegram.ext import Filters
-from telegram.ext import CommandHandler
-from telegram.ext import MessageHandler
+from telegram.ext import CallbackContext, ContextTypes
 
-# This is the Telegram Bot that prints all input documents. It can print pdf and txt files.
+# This is the Telegram Bot that prints all input documents. It uses libreoffice to convert non-pdf files
+# to pdf, which allows printing almost any document formats and images.
 # Before sending files for printing user must enter the password by command "/auth <password>".
 
-# TODO: Store your bot's token key (from Telegram BotFather)
-tokenpath = "./token"
-token_key = open("./token", "r").read().strip()
-password = open("./auth_password", "r").read().strip()
+# Setup instructions: ###############################################################
+# 1. Store your bot's token key (from Telegram BotFather) into the file named below #
+token_path = "./token"                                                              #
+# 2. Store authentication password into the file named below                        #
+password_path = "./auth_password"                                                   #
+#####################################################################################
 
+token_key = open(token_path, "r").read().strip()
+password = open(password_path, "r").read().strip()
+
+# Safety measure. Files larger than this limit are not accepted
 file_size_limit = 64*1024*1024  # 64Mb
 
 # Directory where files for printing should be saved
@@ -46,36 +49,37 @@ def request_auth(update: Update, context: CallbackContext):
 
 
 def start(update: Update, context: CallbackContext):
-    logger.info("User {} started...".format(update.message.from_user.username))
+    logger.info("User {} /start request".format(update.message.from_user.username))
     if not auth_passed(update):
         return request_auth(update, context)
 
-    msg = "You are authorized to print, just send a file here.\nSupported formats: PDF, PS, TXT, JPG.\n"
+    msg = "You are authorized to print, just send a file here.\n"
     msg += "Current state:\n" + os.popen('lpstat -p').read() + "\n"
     msg += "Printer queue:\n" + os.popen('lpq').read()
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def pending(update: Update, context: CallbackContext):
+    logger.info("User {} /pending request".format(update.message.from_user.username))
     if not auth_passed(update):
         return request_auth(update, context)
 
-    logger.info("User {} pending request".format(update.message.from_user.username))
     msg = os.popen('lpstat -W not-completed').read()
     if msg == '':
         msg = "No jobs found"
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def completed(update: Update, context: CallbackContext):
+    logger.info("User {} /completed request".format(update.message.from_user.username))
     if not auth_passed(update):
         return request_auth(update, context)
 
-    logger.info("User {} completed request".format(update.message.from_user.username))
     msg = os.popen('lpstat -W completed | head').read()
     if msg == '':
         msg = "No jobs found"
     context.bot.send_message(chat_id=update.effective_chat.id, text=msg)
 
 def cancel(update: Update, context: CallbackContext):
+    logger.info("User {} /cancel request".format(update.message.from_user.username))
     if not auth_passed(update):
         return request_auth(update, context)
 
@@ -93,7 +97,11 @@ def auth_passed(update):
     return update.message.chat_id in authorized_chats
 
 
+def update_message(context: CallbackContext, msg: telegram.Message, text):
+    context.bot.edit_message_text(text, msg.chat.id, msg.message_id)
+
 def authorize(update: Update, context: CallbackContext):
+    logger.info("User {} /authorize request".format(update.message.from_user.username))
     args = ''.join(context.args)
 
     if auth_passed(update):
@@ -111,22 +119,30 @@ def authorize(update: Update, context: CallbackContext):
 
 
 def cmd_print_file(file_path):
-    os.system('lpr "{}"'.format(file_path))
+    cmd = 'lpr "{}"'.format(file_path)
+    logger.info("Executing {}".format(cmd))
+    os.system(cmd)
 
-def maybe_convert(update: Update, context: CallbackContext, file_path):
+def maybe_convert(context: CallbackContext, msg: telegram.Message, file_path):
     (fpath, ext) = os.path.splitext(file_path);
     if ext.lower() == ".pdf":
         return (file_path, True)
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Converting to pdf...")
+    update_message(context, msg, "Converting to pdf...")
 
-    if 0 == os.system('timeout 5 libreoffice --convert-to "pdf" "{}" --outdir {}'.format(file_path, files_dir)):
+    if 0 == os.system('timeout 30 libreoffice --convert-to "pdf" "{}" --outdir {}'.format(file_path, files_dir)):
         new_path = files_dir + '/' + os.path.splitext(os.path.basename(file_path))[0] + '.pdf'
         return (new_path, True )
     else:
         return (file_path, False)
 
-def print_file(update: Update, context: CallbackContext):
+def get_num_pages(file_path):
+    num_pages = os.popen('pdfinfo "{}" | grep Pages'.format(file_path)).read().strip()
+    return int(''.join(filter(str.isdigit, num_pages)))
+
+
+def upload_file(update: Update, context: CallbackContext):
+    logger.info("User {} file upload".format(update.message.from_user.username))
     if not auth_passed(update):
         return request_auth(update, context)
     if update.message.document is not None:
@@ -145,7 +161,7 @@ def print_file(update: Update, context: CallbackContext):
         context.bot.send_message(chat_id=update.effective_chat.id, text="File is too big ({} > {})!".format(file_size, file_size_limit))
         return
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Downloading file...")
+    reply_msg = context.bot.send_message(chat_id=update.effective_chat.id, text="Downloading file...")
 
     file_path = files_dir + '/' + file_name
 
@@ -154,32 +170,70 @@ def print_file(update: Update, context: CallbackContext):
     new_file.download(file_path)
     logger.info("Downloaded  file {} from {}!".format(file_name, update.message.from_user.username))
 
-    (file_path, success) = maybe_convert(update, context, file_path)
+    (file_path, success) = maybe_convert(context, reply_msg, file_path)
     if not success:
-        context.bot.send_message(chat_id=update.effective_chat.id, text="Failed to convert file {}, size {}!".format(file_name, file_size))
+        update_message(context, reply_msg, "Failed to convert file {}, size {}!".format(file_name, file_size))
         return
 
-    num_pages = os.popen('pdfinfo "{}" | grep Pages'.format(file_path)).read().strip()
-    num_pages = int(''.join(filter(str.isdigit, num_pages)))
+    # Delete upload status message after successful upload and convert
+    context.bot.delete_message(reply_msg.chat.id, reply_msg.message_id)
 
-    logger.info("Printing file {}. Number of pages: {}...".format(file_path, num_pages))
+    num_pages = get_num_pages(file_path)
+
+    keyboard = [
+        [telegram.InlineKeyboardButton("Print", callback_data="print {}".format(file_path))],
+        [telegram.InlineKeyboardButton("Delete file", callback_data="delete {}".format(file_path))],
+    ]
+    reply_markup = telegram.InlineKeyboardMarkup(keyboard)
+
+    update.message.reply_text("Num pages: {}".format(num_pages), reply_markup=reply_markup)
+
+
+def check_file(file_path):
+    return file_path == files_dir + '/' + os.path.basename(file_path)
+
+def button(update: Update, context: CallbackContext):
+    query = update.callback_query
+    logger.info("User {} clicked button: ".format(query.data))
+    if not auth_passed(query):
+        return request_auth(update, context)
+
+    # CallbackQueries need to be answered, even if no notification to the user is needed
+    # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
+    query.answer()
+
+    (cmd, file_path) = query.data.split(' ', 1)
+    if not check_file(file_path):
+        return
+
+    if cmd == 'delete':
+        os.unlink(file_path)
+        update_message(context, query.message, "Deleted")
+    elif cmd == 'print':
+        print_file(context, query.message, file_path)
+    else:
+        update_message(context, query.message, f"WAT?")
+
+
+def print_file(context: CallbackContext, msg: telegram.Message, file_path):
+    num_pages = get_num_pages(file_path)
+    logger.info("Printing file {}. Number of pages: {}".format(file_path, num_pages))
     cmd_print_file(file_path)
-
-    logger.info("File {} sent for printing. Number of pages: {}".format(file_path, num_pages))
-    context.bot.send_message(chat_id=update.effective_chat.id, text="File sent for printing!")
+    update_message(context, msg, "File was sent for printing!")
 
 
 def main():
-    updater = Updater(token=token_key)
+    updater = telegram.ext.Updater(token=token_key)
     dispatcher = updater.dispatcher
 
-    dispatcher.add_handler(CommandHandler('start', start))
-    dispatcher.add_handler(CommandHandler('auth', authorize))
-    dispatcher.add_handler(CommandHandler('pending', pending))
-    dispatcher.add_handler(CommandHandler('completed', completed))
-    dispatcher.add_handler(CommandHandler('cancel', cancel))
-    dispatcher.add_handler(MessageHandler(filters=Filters.document, callback=print_file))
-    dispatcher.add_handler(MessageHandler(filters=Filters.photo, callback=print_file))
+    dispatcher.add_handler(telegram.ext.CommandHandler('start', start))
+    dispatcher.add_handler(telegram.ext.CommandHandler('auth', authorize))
+    dispatcher.add_handler(telegram.ext.CommandHandler('pending', pending))
+    dispatcher.add_handler(telegram.ext.CommandHandler('completed', completed))
+    dispatcher.add_handler(telegram.ext.CommandHandler('cancel', cancel))
+    dispatcher.add_handler(telegram.ext.MessageHandler(filters=telegram.ext.Filters.document, callback=upload_file))
+    dispatcher.add_handler(telegram.ext.MessageHandler(filters=telegram.ext.Filters.photo, callback=upload_file))
+    dispatcher.add_handler(telegram.ext.CallbackQueryHandler(button))
 
     logger.info("Listening...")
     updater.start_polling()
